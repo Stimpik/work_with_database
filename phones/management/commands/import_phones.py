01d9df0fe84e6c52404d2b112ebf098c0497f7f9
Igor D <iderik@mail.ru>
2023-03-29 23:25:14 +0300

    Initial commit

---
+++ phones/management/commands/import_phones.py
@@ -0,0 +1,24 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as file:
+            phones = list(csv.DictReader(file, delimiter=';'))
+
+        for phone in phones:
+            new_phone = Phone(id=phone['id'],
+                              name=phone['name'],
+                              image=phone['image'],
+                              release_date=phone['release_date'],
+                              price=phone['price'],
+                              lte_exists=phone['lte_exists'],
+                              slug=phone['name'].replace(' ', '-')
+                              )
+            new_phone.save()
---
+++ phones/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.CharField(max_length=100)
+    image = models.URLField()
+
+
